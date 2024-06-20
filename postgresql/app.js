c748d7564ba163c31eaaab44047d96c25e2e7a92
Son Nguyen <hoangson091104@gmail.com>
2024-06-20 11:57:50 +0700

    Update: Fix mongo-redis flow

---
+++ postgresql/app.js
@@ -0,0 +1,20 @@
+const config = require('./config');
+const { Client } = require('pg');
+
+// Create a new client instance
+const client = new Client(config);
+
+// Connect to the database
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Connection error', err.stack));
+
+// Example query
+client.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.rows);
+    }
+    client.end(); // Close the connection
+});
